50118f7228e975a2b02ad952a652c0ee6eeab6df
Simple-temp <fiveriam01@gmail..com>
2022-02-09 17:24:23 +0600

    some changes

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./About.css"
 import ProgressBar from "@ramonak/react-progress-bar";
-import person from "../../../img/p1.jpg"
+import person from "../../../img/a1.jpg"
 import { Typewriter } from 'react-simple-typewriter'
 
 
@@ -10,7 +10,7 @@ const About = () => {
         <section>
             <div className="container inner-container mt-1 py-5" id="about">
                 <div className="row">
-                    <div className="col-lg-6 col-md-6 col-12 about">
+                    <div className="col-lg-6 col-md-6 col-12 about d-flex align-items-center">
                         <img src={person} alt="" className='img-fluid' />
                     </div>
                     <div className="col-lg-6 col-md-6 col-12">
@@ -30,7 +30,6 @@ const About = () => {
                                         />
                                     </span>
                                 </small>
-                                <p className='mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta magni perspiciatis veniam dolores ratione consequuntur mollitia itaque distinctio atque corporis.</p>
                             </div>
                             <div className="p-bar">
                                 <label>React Js</label>
--- src/Pages/Home/Main/Main.js
+++ src/Pages/Home/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Main.css"
-import person from "../../../img/p.jpg"
+import person from "../../../img/p1.jpg"
 import { Typewriter } from 'react-simple-typewriter'
 
 
@@ -9,14 +9,14 @@ const Main = () => {
     return (
         <div className='container mt-5 py-5 main w-75 mx-auto'>
             <div className="row">
-                <div className="col-lg-5 col-md-6 col-sm-12 img">
-                    <img src={person} alt="" />
+                <div className="col-lg-6 col-md-6 col-sm-12 img">
+                    <img src={person} alt="" className='img-fluid' />
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center">
-                    <div className="main-box py-5">
-                        <h4>Hello <br />I am a <span>
+                    <div className="main-box">
+                        <h4>Hello <br />I am <span>
                             <Typewriter
-                                words={['Web Developer', 'Web Desinger', 'Front End Developer']}
+                                words={['Aziz..','A Web Developer', 'A Web Desinger', 'A Front-End Developer']}
                                 loop
                                 cursor
                                 cursorStyle='_'
@@ -26,7 +26,7 @@ const Main = () => {
                             />
                         </span></h4>
                         <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quod cum beatae corrupti, itaque dolor.</p>
-                        <button className='custom-btn'>Hire me!</button>
+                        <button className='custom-btn'>Hire me!</button><button className='custom-btn but'>Explore Expreience</button>
                     </div>
                 </div>
             </div>
